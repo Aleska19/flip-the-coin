1399a6fdb38f01e694072c108024c173479461b2
Aleska <cegarraaleska91@gmail.com>
2025-07-01 14:23:31 -0300

    Agrgando fucnionalidad al boton

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+const flipBtn = document.getElementById('flipBtn');
+const coinImg = document.getElementById('coinImg');
+const resultadoText = document.getElementById('resultado')
+
+
+
+
+function lanzarMoneda  () {
+    const isHeads = Math.random() < 0.5;
+    const imagePath = isHeads ? '../resources/heads.svg' : '../resources/tails.svg';
+    const resultado = isHeads ? 'Heads' : 'Tails';
+
+    resultadoText.textContent =  resultado;
+    coinImg.src = imagePath;
+
+
+
+};
+flipBtn.addEventListener('click', lanzarMoneda);
+coinImg.addEventListener('click', lanzarMoneda);
+
